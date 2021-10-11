ee6817512e2d738b64abd34a66b27a6b2d39db9a
namihira33 <syuntakosu1011@gmail.com>
2021-10-11 16:48:15 +0900

    プッシュ先確認確認

--- src/run.py
+++ src/run.py
@@ -5,7 +5,7 @@ import numpy as np
 seeds = list(np.random.choice(10000,3))
 
 c = {
-    'model_name': 'Resnet18','bs': 64,'n_epoch': 120,
+    'model_name': 'Resnet18','bs': 128,'n_epoch': 120,
     'lr': [7e-5,8e-5,9e-5,1e-4,2e-4,3e-4,4e-4,5e-4,6e-4,7e-4,8e-4,9e-4],'seed': seeds
 }
 
